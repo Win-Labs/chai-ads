52ac33d318a714a8d881fa62b86d7888e69e4ae8
gylman <gylmanbm@gmail.com>
2024-07-20 08:12:13 +0900

    import table, copy, arrow, totaltxs components

---
+++ client/src/components/TotalTxs.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Table from "./Table";
+import { useTxs } from "../contexts/TxsContext";
+
+const TotalTxs = () => {
+  const { txs } = useTxs();
+
+  const headers = [
+    "user",
+    "encrypted",
+    "decrypted",
+    "sequencer",
+    "block",
+    "order",
+    "rollup",
+    "operator",
+    "status",
+  ];
+
+  const entries = txs.map(
+    ({ user, encrypted, decrypted, block, order, rollup, status }) => {
+      return {
+        user,
+        encrypted,
+        decrypted,
+        sequencer: block.sequencer,
+        block: block.height,
+        order,
+        rollup: rollup.title,
+        operator: rollup.operator,
+        status,
+      };
+    }
+  );
+
+  return <Table headers={headers} entries={entries} />;
+};
+
+export default TotalTxs;
