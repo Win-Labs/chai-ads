aa6b6ab9641320698e58b1a602c065907f5447bf
gylman <gylmanbm@gmail.com>
2024-07-30 07:43:24 +0900

    test ipfs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,13 @@ import FormModal from "./components/FormModal";
 import TotalTxs from "./components/Outlet";
 import RootLayout from "./components/Root";
 import Root from "./components/Root";
+import TestIPFS from "./components/TestIPFS";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <Root />;
+  // return <Root />;
+  return <TestIPFS />;
 }
 
 export default App;
