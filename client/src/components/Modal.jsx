2058089b038429b7e2866447b0721713e58b7ce4
gylman <gylmanbm@gmail.com>
2024-10-24 20:30:07 +0900

    ref: remove redundancy

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -22,25 +22,16 @@ import { usePATCH } from "../hooks/useServer";
 const Modal = ({ toggle }) => {
   const { address } = useAccount();
 
-  // Step 1
+  const [img, setImage] = useState("");
   const [recipient, setRecipient] = useState("orkhan_influenza");
   const [duration, setDuration] = useState(30);
   const [currency, setCurrency] = useState("usdt");
   const [network, setNetwork] = useState("ethereum");
-  // Step 2
+
   const [text, setText] = useState("rollup_id");
   const [amount, setAmount] = useState(1000);
-  const [rollupType, setRollupType] = useState("polygon_cdk");
-  const [orderCommitmentType, setOrderCommitmentType] = useState("sign");
-  const [encryptedTransactionType, setEncryptedTransactionType] =
-    useState("skde");
-  const [platform, setPlatform] = useState("ethereum");
-  const [serviceProvider, setServiceProvider] = useState("eigen_layer");
-  const [rpcUrl, setRpcUrl] = useState("https://www.google.ru/");
-  const [webSocketUrl, setWebSocketUrl] = useState("https://www.naver.com/");
-  const [blockExplorerUrl, setBlockExplorerUrl] = useState(
-    "https://www.hello-world.com/"
-  );
+  const [ipfsUrl, setIpfsUrl] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
 
   const [step, setStep] = useState(1);
   const [transactionCompleted, setTransactionCompleted] = useState(false); // New state to track transaction completion
@@ -106,13 +97,8 @@ const Modal = ({ toggle }) => {
 
     // Only proceed if the transaction is not pending and it hasn't already triggered a step transition
     if (hash && !isHashPending && !transactionCompleted) {
-      if (step === 1) {
-        setStep(2);
-      } else if (step === 2) {
-        setStep(3);
-      } else if (step === 3) {
-        toggle();
-      }
+      toggle();
+
       setTransactionCompleted(true); // Mark the transaction as completed, preventing multiple triggers
     }
   }, [hash, isHashPending, step, transactionCompleted]);
@@ -127,66 +113,64 @@ const Modal = ({ toggle }) => {
         <Title>
           <span>Request Ad</span>
         </Title>
-        {isHashPending ? (
+        {!submitted || !ipfsUrl ? (
           <Loader />
         ) : (
-          step === 1 && (
-            <>
-              <InputContainer>
-                <Label>Recipient</Label>
-                <Input
-                  value={recipient}
-                  type="text"
-                  onChange={(e) => {
-                    setRecipient(e.target.value);
-                  }}
-                />
-              </InputContainer>{" "}
-              <InputContainer>
-                <Label>Duration</Label>
-                <Input
-                  value={duration}
-                  type="text"
-                  onChange={(e) => {
-                    setDuration(e.target.value);
-                  }}
-                />
-              </InputContainer>{" "}
-              <InputContainer>
-                <Label>Text</Label>
-                <Input
-                  value={text}
-                  type="text"
-                  onChange={(e) => {
-                    setText(e.target.value);
-                  }}
-                />
-              </InputContainer>
-              <InputContainer>
-                <Label>Currency</Label>
-                <SelectBox onChange={(e) => setCurrency(e.target.value)}>
-                  <option defaultValue="usdt">USDT</option>
-                  <option value="eth">ETH</option>
-                </SelectBox>
-              </InputContainer>
-              <InputContainer>
-                <Label>Offer</Label>
-                <Input
-                  value={amount}
-                  type="text"
-                  onChange={(e) => {
-                    setAmount(e.target.value);
-                  }}
-                />
-              </InputContainer>
-              <InputContainer>
-                <Label>Network</Label>
-                <SelectBox onChange={(e) => setNetwork(e.target.value)}>
-                  <option defaultValue="ethereum">Ethereum Mainnet</option>
-                </SelectBox>
-              </InputContainer>{" "}
-            </>
-          )
+          <>
+            <InputContainer>
+              <Label>Recipient</Label>
+              <Input
+                value={recipient}
+                type="text"
+                onChange={(e) => {
+                  setRecipient(e.target.value);
+                }}
+              />
+            </InputContainer>{" "}
+            <InputContainer>
+              <Label>Duration</Label>
+              <Input
+                value={duration}
+                type="text"
+                onChange={(e) => {
+                  setDuration(e.target.value);
+                }}
+              />
+            </InputContainer>{" "}
+            <InputContainer>
+              <Label>Text</Label>
+              <Input
+                value={text}
+                type="text"
+                onChange={(e) => {
+                  setText(e.target.value);
+                }}
+              />
+            </InputContainer>
+            <InputContainer>
+              <Label>Currency</Label>
+              <SelectBox onChange={(e) => setCurrency(e.target.value)}>
+                <option defaultValue="usdt">USDT</option>
+                <option value="eth">ETH</option>
+              </SelectBox>
+            </InputContainer>
+            <InputContainer>
+              <Label>Offer</Label>
+              <Input
+                value={amount}
+                type="text"
+                onChange={(e) => {
+                  setAmount(e.target.value);
+                }}
+              />
+            </InputContainer>
+            <InputContainer>
+              <Label>Network</Label>
+              <SelectBox onChange={(e) => setNetwork(e.target.value)}>
+                <option defaultValue="ethereum">Ethereum Mainnet</option>
+              </SelectBox>
+            </InputContainer>{" "}
+          </>
         )}
         <Buttons>
           <SubmitBtnContainer>
